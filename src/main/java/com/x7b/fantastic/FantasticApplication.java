b3b655487a6e927f7426adf662b6ae5d16857dc3
Joseph Tam <jtktam@gmail.com>
2018-12-13 10:56:22 -0500

    initial commit

---
+++ src/main/java/com/x7b/fantastic/FantasticApplication.java
@@ -0,0 +1,14 @@
+package com.x7b.fantastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasticApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/x7b/fantastic/FantasticApplicationTests.java
@@ -0,0 +1,17 @@
+package com.x7b.fantastic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FantasticApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
